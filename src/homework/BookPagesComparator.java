494f7eb8d57927a60537598cc7934f34702807df
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-23 00:54:46 +0100

    Override compare()

--- src/homework/BookPagesComparator.java
+++ src/homework/BookPagesComparator.java
@@ -12,4 +12,17 @@ public class BookPagesComparator implements Comparator<Book> {
     this.ignoreCase = ignoreCase;
   }
 
+  @Override
+  public int compare(Book b1, Book b2) {
+    if ( b1.getPages() != b2.getPages()){
+      return b2.getPages()- b1.getPages();
+    }
+    String title1 = b1.getTitle();
+    String title2 = b2.getTitle();
+    if (ignoreCase) {
+      title1 = title1.toLowerCase();
+      title2 = title2.toLowerCase();
+    }
+    return title1.compareTo(title2);
+  }
 }
