0df9b35dbee788eb56aaf94b5e3a21b34ec895c6
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-23 00:21:01 +0100

    Initial commit - create BookTitleComparator

---
+++ src/homework/BookTitleComparator.java
@@ -0,0 +1,11 @@
+package homework;
+
+
+import java.util.Comparator;
+
+// Создайте компаратор, который позволит сортировать книги по названию,
+// при одинаковых названиях - по автору (и там, и там - по алфавиту)
+public class BookTitleComparator implements Comparator<Book> {
+
+
+}
---
+++ src/homework/HomeworkMain.java
@@ -0,0 +1,23 @@
+package homework;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class HomeworkMain {
+
+  // Создайте компаратор, который позволит сортировать книги по названию,
+  // при одинаковых названиях - по автору (и там, и там - по алфавиту)
+  public static void main(String[] args) {
+    List<Book> books = new ArrayList<>();
+
+    books.add(new Book("Author 3", "Book 3", 250));
+    books.add(new Book("Author 1", "Book 2", 251));
+    books.add(new Book("Author 3", "Book 1", 252));
+
+    Collections.sort(books);
+    for (Book b : books) {
+      System.out.println(b);
+    }
+  }
+}
