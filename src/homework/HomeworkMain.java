4d28efdbfd0f989750b7907fe92f7b9480fcd3d5
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-23 00:41:24 +0100

    Use BookTitleComparator

--- src/homework/HomeworkMain.java
+++ src/homework/HomeworkMain.java
@@ -13,10 +13,16 @@ public class HomeworkMain {
 
     books.add(new Book("Author 3", "Book 3", 250));
     books.add(new Book("Author 1", "Book 2", 251));
-    books.add(new Book("Author 3", "Book 1", 252));
+    books.add(new Book("Author 2", "Book 1", 252));
 
     Collections.sort(books);
     for (Book b : books) {
+      System.out.println(b);
+    }
+
+    System.out.println("Книги по названию (по автору) по алфавиту");
+    books.sort(new BookTitleComparator(true));
+    for (Book b : books){
       System.out.println(b);
     }
   }
