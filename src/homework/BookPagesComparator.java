b2e246923862127856d846e747eec03c64af98cf
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-23 00:44:04 +0100

    Create class BookPagesComparator

---
+++ src/homework/BookPagesComparator.java
@@ -0,0 +1,8 @@
+package homework;
+
+import java.util.Comparator;
+
+// Создайте компаратор, который позволит сортировать книги по убыванию количества страниц,
+// а при совпадающем объёме - по названиям ("по возрастанию", по алфавиту).
+public class BookPagesComparator implements Comparator<Book> {
+}
