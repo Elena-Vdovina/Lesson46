a78f7176df332f00f75c00a11a4135cdf2f74380
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-23 00:33:19 +0100

    Override compare()

--- src/homework/BookTitleComparator.java
+++ src/homework/BookTitleComparator.java
@@ -12,4 +12,24 @@ public class BookTitleComparator implements Comparator<Book> {
   public BookTitleComparator(boolean ignoreCase) {
     this.ignoreCase = ignoreCase;
   }
-}
+
+  @Override
+  public int compare(Book b1, Book b2) {
+    String title1 = b1.getTitle();
+    String title2 = b2.getTitle();
+    if (ignoreCase) {
+      title1 = title1.toLowerCase();
+      title2 = title2.toLowerCase();
+    }
+    if (title1.equals(title2)) {
+      return title1.compareTo(title2);
+    }
+    String author1 = b1.getAuthor();
+    String author2 = b2.getAuthor();
+    if (ignoreCase) {
+      author1 = author1.toLowerCase();
+      author2 = author2.toLowerCase();
+    }
+    return author1.compareTo(author2);
+    }
+  }
